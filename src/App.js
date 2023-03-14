ccdaa61f9a55f47f83395f6d9777b2e7fde6cd9d
DominikLau <84925544+DominikLau@users.noreply.github.com>
2023-03-14 16:55:23 +0100

    Small Updates: Added if / else for checking if products are available, Small HTML changes

--- src/App.js
+++ src/App.js
@@ -11,31 +11,36 @@ import Banner from './images/general/Banner.png'
 import Logout from './images/general/logout.png'
 
 
+
 class App extends React.Component {
 
   constructor() {
     super();
     this.state ={
-      value: 30
+      value: {value}
     };
   }
 
   clickMe = () => {
-    alert("Der Shop ist zurzeit leider nicht verfügbar!");
-    alert(value);
-    this.setState({value: value -= 1});
+    if (value >= 1) {
+      alert("Die Bestellung wurde dem Warenkorb hinzugefügt!");
+      this.setState({value: value -= 1});
+    }
+    else {
+      alert("Wir haben leider keine Produkte mehr auf Lager tut uns leid! :(")
+      }
   }
 
     render(){
       return(
       <div className="App">
       <div class="topnav">
-        <a class="picture" href="#home"><img src={Flasche} className="App-logo_fl" alt="logo-abbildung"/></a>
+        <pic class="picture" href="#home"><img src={Flasche} className="App-logo_fl" alt="logo-abbildung"/></pic>
         <a href="#news">Neuigkeiten</a>
         <a href="#contact">Kontakt</a>
         <a href="#about">Über uns</a>
         <a href="#shop">Kaufe jetzt deine Flasche!</a>
-        <logout class="picture" href="#loginlogout"><img src={Logout} className="Logout" alt="logo-abbildung"/></logout>
+        <logout href="#loginlogout"><img src={Logout} className="Logout" alt="logo-abbildung"/></logout>
       </div>
 
         <a href="/"><img src={Banner} className="App-banner" alt="logo der Flasche"/></a>
@@ -65,18 +70,19 @@ class App extends React.Component {
 
 
 const Button = styled.button`
-  background-color: blue;
+  background-color: rgb(79, 166, 71);
   color: white;
   font-size: 12px;
   padding: 5px 20px;
   border-radius: 5px;
-  margin: 10px 0px;
-  cursor: pointer
-  outline: 0
-  box-shadow: 0px 2px 2px lightgray;
+  margin: 10px 5px;
+  cursor: pointer;
+  outline: 0;
+  box-shadow: 2px 2px 0px lightgray;
 `;
 
-var value = 30;
+//Falls ihr den State für Value ändern wollt ändert das hier
+var value = 5;
 
 
 export default App;
