dd42a383c7ce2e6f70a7f4b0bc2d079af0151544
DominikLau <84925544+DominikLau@users.noreply.github.com>
2023-03-14 11:08:32 +0100

    Render now works

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ import styled from "styled-components";
 import './App.css';
 import './Titlebar.css';
 import { clickTest } from "./ClickFunctionTest";
-import WMT from './Wise_mystical_tree.jpg';
+//import WMT from './Wise_mystical_tree.jpg';
 import Flasche from './Flaschenlogo.png';
 import Logo from './CompanyLogo.png';
 import Banner from './_Banner_Neu.png'
-import Logout from './logout.png'
+//import Logout from './logout.png'
 
-const Button = styled.button`
-  background-color: blue;
-  color: white;
-  font-size: 12px;
-  padding: 5px 20px;
-  border-radius: 5px;
-  margin: 10px 0px;
-  cursor: pointer
-  outline: 0
-  box-shadow: 0px 2px 2px lightgray;
-`;
-
-var value = 25;
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state ={
+      value: 30
+    };
+  }
+
+  clickMe = () => {
+    alert("Der Shop ist zurzeit leider nicht verfügbar!");
+    alert(value);
+    this.setState({value: value -= 1});
+  }
 
     render(){
       return(
@@ -44,7 +44,7 @@ class App extends React.Component {
           <p>
             <code>Jetzt vorbestellen!</code>
           </p>
-            <Button onClick={clickMe}>Für 39,99€ bestellen</Button>
+            <Button onClick={this.clickMe}>Für 39,99€ bestellen</Button>
             <button type="button" onClick={clickTest}> Test </button>
           <p>
             <code>Noch im Lager: {value} Stück</code>
@@ -56,21 +56,27 @@ class App extends React.Component {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Das wird die 1,0 Jungs
+          Test Test
         </a> 
     </div>
       );
     }
   }
 
-function clickMe() {
-  alert("Der Shop ist zurzeit leider nicht verfügbar!");
-  alert(value);
-  value -= 1;
-  alert(value);
-  //root.render(value)
-  return value
-}
+
+const Button = styled.button`
+  background-color: blue;
+  color: white;
+  font-size: 12px;
+  padding: 5px 20px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer
+  outline: 0
+  box-shadow: 0px 2px 2px lightgray;
+`;
+
+var value = 30;
 
 
 export default App;
