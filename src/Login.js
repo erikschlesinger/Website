844a2830caa3ba2c30b5e1ec750d52ff24cf2533
DominikLau <84925544+DominikLau@users.noreply.github.com>
2023-03-23 11:58:04 +0100

    Update Login.js
    Added Input data save on Login

--- src/Login.js
+++ src/Login.js
@@ -2,8 +2,23 @@ import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./Login.css"
 import "./index.js"
+import { Navigate } from "react-router-dom";
+
+
+
 
 class Login extends React.Component {
+
+  saveInput = () => {
+    var inputEm = document.getElementById("userEmail").value;
+    var inputPw = document.getElementById("userPassword").value;
+    alert(inputEm);
+    alert(inputPw);
+    if (inputEm == "hello@gmx.de" && inputPw == "root") {
+      alert("Test");
+      //return <Navigate to="/" replace={true} />;
+    }
+  }
 
   render() {
     return(
@@ -16,6 +31,7 @@ class Login extends React.Component {
               <div className="form-group mt-3">
                 <label>Email address</label>
                 <input
+                  id="userEmail"
                   type="email"
                   className="form-control mt-1"
                   placeholder="Enter email" />
@@ -23,12 +39,13 @@ class Login extends React.Component {
               <div className="form-group mt-3">
                 <label>Password</label>
                 <input
+                  id="userPassword"
                   type="password"
                   className="form-control mt-1"
                   placeholder="Enter password" />
               </div>
               <div className="d-grid gap-2 mt-3">
-                <button type="submit" className="btn btn-primary">
+                <button type="submit" className="btn btn-primary" onClick={this.saveInput}>
                   Login
                 </button>
               </div>
